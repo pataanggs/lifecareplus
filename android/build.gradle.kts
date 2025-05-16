0b97d7eab4d1dbd1706bda71ea812b2158be5526
Fathan Andi Kartagama <81012125+pataanggs@users.noreply.github.com>
2025-05-16 15:06:00 +0700

    gatau gue nyerah

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,17 +1,33 @@
 buildscript {
+    // Define the Kotlin version as a property using extra
+    extra.set("kotlin_version", "2.1.21") // Use stable version
+
+    repositories {
+        google()
+        mavenCentral()
+    }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.2.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
-        classpath("com.google.gms:google-services:4.4.0") // Versi stabil
+        classpath("com.google.gms:google-services:4.4.2")
+        classpath("com.android.tools.build:gradle:8.1.0") // Stable version
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.21") // Use stable version
     }
 }
 
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://storage.googleapis.com/download.flutter.io") } // For Flutter engine artifacts
+    }
+}
+
+// Configure build directory
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
-rootProject.layout.buildDirectory.value(newBuildDir)
+rootProject.layout.buildDirectory.set(newBuildDir)
 
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
+    project.layout.buildDirectory.set(newSubprojectBuildDir)
 }
 
 subprojects {
--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,4 +1,14 @@
 pluginManagement {
+    val flutterSdkPath = run {
+        val properties = java.util.Properties()
+        file("local.properties").inputStream().use { properties.load(it) }
+        val flutterSdkPath = properties.getProperty("flutter.sdk")
+        require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
+        flutterSdkPath
+    }
+
+    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+
     repositories {
         google()
         mavenCentral()
@@ -6,13 +16,21 @@ pluginManagement {
     }
 }
 
+// TAMBAHKAN INI
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://storage.googleapis.com/download.flutter.io") }
     }
 }
 
-rootProject.name = "lifecareplus"
+plugins {
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    id("com.android.application") version "8.7.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.21" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
+}
+
 include(":app")
